f19c65ef993752bf1af0b9f6e34f4f781829f369
Kadu <eduarduo.aquino@gmail.com>
2024-08-27 17:01:23 -0300

    Versao inicial login

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from '../pages/Cardapio';
+import LoginPage from '../pages/Login';
 
 export function App() {
   return (
     <Router>
       <Routes>
         <Route path="/cardapio" element={<Cardapio />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
