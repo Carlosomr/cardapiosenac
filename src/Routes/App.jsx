ca9fb3881e986540690ef2fd2cc05b0bb7e5159e
Carlos Eduardo <carloseduardo.moura.contato@gmail.com>
2024-09-05 17:23:26 -0300

    outro teste vercel

--- src/Routes/App.jsx
+++ src/Routes/App.jsx
@@ -1,25 +1,20 @@
+// Routes/App.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Cardapio from '../pages/Cardapio';
 import Login from '../pages/Login';
 import Produtos from '../pages/Produtos';
 import Registro from '../pages/Registro';
 
-export function App() {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/cardapio" element={<Cardapio />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Produtos />} />
-        <Route path="/registro" element={<Registro />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Cardapio />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/administrador" element={<Produtos />} />
+      <Route path="/registro" element={<Registro />} />
+    </Routes>
   );
 }
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-);
 
- 
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import {ThemeProvider} from 'styled-components';
-import GlobalStyles from "./styles/global"
-
-import theme from './styles/theme'
-
-
-import { App } from './Routes/App.jsx';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from "./styles/global";
+import theme from './styles/theme';
+import App from './Routes/App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}> 
-     <GlobalStyles/>
-     <App />
-     <BrowserRouter/>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
-    
-
   </React.StrictMode>,
-)
+);
